6d1bc4e3b953a13eb46903389181c698d627bca7
nuxarch <nuxarch@gmail.com>
2024-09-12 11:49:01 +0700

    week 3 finished

---
+++ week3/00_week3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int isiTangki = 10;
+double nilaiSuhu = 50.55;
+char nilaiUAS = 'A';
+string predikatKelas = "sangat baik";
+bool statusMahasiswa = true;
+
+int nilai1, nilai2, nilai3;
+
+int decimal;
+int _string;
+string status = "asdf sdf";
+bool nilai5;
+char predikat = 'C';
+
+const int detikPerMenit = 60;
+int main()
+{
+    cout << "isi var tangki adalah : " << isiTangki << "liter" << endl;
+    cout << "isi var nilai suhu adalah : " << nilaiSuhu << " Celc" << endl;
+    cout << "isi var nilai UAS adalah : " << nilaiUAS << "(unggul)" << endl;
+    cout << "isi var predikat Kelas adalah : " << predikatKelas << "memuaskan" << endl;
+    cout << "isi var status Mahasiswa adalah : " << statusMahasiswa << "(aktif)" << endl;
+
+    nilai1 = 50;
+    nilai2 = 50;
+    nilai3 = 100;
+    cout << "nilai total :" << nilai1 + nilai2 + nilai3 << endl;
+    // detikPerMenit = 10;
+    return 1;
+}
